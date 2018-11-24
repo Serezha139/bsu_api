2199fcc5fc5c3d90d8c5f44009286820e27716b6
ivanov.grigory <grisha139@tut.by>
2018-11-24 13:49:44 +0300

    Add game repr and ongoing

--- games/models.py
+++ games/models.py
@@ -10,8 +10,20 @@ STAGE_CHOICES = (
     ("1", "Финал"),
 )
 
+SEASON_CHOICES = (
+    (2018, '2018'),
+    (2019, '2019')
+)
+
+
 class Game(models.Model):
     game_datetime = models.fields.DateTimeField()
     teams = models.ManyToManyField(Team)
     stage = models.fields.CharField(choices=STAGE_CHOICES, max_length=40)
     description = models.fields.TextField()
+    season = models.fields.IntegerField(choices=SEASON_CHOICES, null=True)
+    ongoing = models.fields.BooleanField(default=False)
+
+
+    def __str__(self):
+        return "%s %s" % (str(self.season), self.get_stage_display())
